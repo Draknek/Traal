622438a998d0c320b40f971d0a42a026cda2b251
Draknek <alan@draknek.org>
2012-12-14 01:10:14 +0000

    Add some AIR stuff

---
+++ air/borderify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for size in 36 48 72 96
+do
+	offset=$(($size / 36))
+	offset2=$(($size - $offset - 1))
+	radius=$(($size / 12))
+	input=${size}x${size}.png
+	output=${size}x${size}-border.png
+	echo "Converting $input"
+	convert -size ${size}x${size} xc:none -fill white -draw \
+		"roundrectangle $offset,$offset $offset2,$offset2 $radius,$radius" \
+		$input -compose SrcIn -composite $output
+done
